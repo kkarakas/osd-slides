68a6ce6a76c4276917512ea48394c998806f8cfb
kkarakas <kkarakas@fandm.edu>
2023-04-11 03:28:37 -0400

    auto-updating docs

--- osd_slides/__main__.py
+++
@@ -1,6 +0,0 @@
-from osd_slides.main import fefmalsmc
-
-if __name__ == "__main__":
-    # TODO dont know how package this python code into a
-    # service run from terminal this will do for now
-    fefmalsmc()
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup()
-# setup(name="osd_slides")
-# from setuptools import setup, find_packages
-#
-# setup(name="osd_slides", packages=find_packages())
