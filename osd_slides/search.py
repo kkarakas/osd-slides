21894f28f75d1bac18270c24d9fb17812f8106af
kkarakas <kkarakas@fandm.edu>
2023-05-10 18:20:09 -0400

    tests added

--- osd_slides/search.py
+++ osd_slides/search.py
@@ -15,7 +15,7 @@ class Search:
         self.slides = slides
         self.url = url # TODO this should be read from properties file
 
-        for z,slide in enumerate(slides):
+        for z, slide in enumerate(slides):
             response = requests.get(url+slide)
             soup = BeautifulSoup(response.text, "html.parser")
             table = soup.find("div", class_="slides")
@@ -23,7 +23,7 @@ class Search:
 
             for idx, section in enumerate(sections):
 
-                if "data-separator" in section.attrs:
+                if "data-separator" in section.attrs: #has multiple pages in one x axis value
                     seperator = section["data-separator"]
                     non_seperated_text = section.contents[1].text #current all the stuff
 
@@ -32,28 +32,32 @@ class Search:
                         temp = re.sub("(<!--.*?-->)", "",individual_text, flags=re.DOTALL) # removes html comments
                         self.text[z][idx][yindx] = re.sub(r"\s+", " ", temp).strip() # removes empty spaces
                 else: # only has one page in that
-                    # section.contents[1].text.remove('\n')
-                    # self.text[idx][0] = section.contents[1].extract()
                     temp = re.sub("(<!--.*?-->)", "",  section.contents[1].text, flags=re.DOTALL) # removes html comments
                     self.text[z][idx][0] = re.sub(r"\s+", " ", temp).strip() # removes empty spaces
-            print(self.text)
-            #clean_text = re.sub(r"\s+", " ", text).strip()
             #https://stackoverflow.com/questions/761824/python-how-to-convert-markdown-formatted-text-to-text
-            #TODO remove markdown stuff
-    def lookup(self,keyword):
+
+    def lookup(self, keyword):
         #it will look up the word in the dict and come up with possible answers
+        self.answers = [] # resetted
         for i in range(len(self.text)):
             for idx,outer in self.text[i].items():
                 for yidx, inner in outer.items():
                     if keyword in inner:
                         self.answers.append(tuple([idx, yidx, self.slides[i]]))
-
-        print("Slides that can be found are")
-        print(self.answers)
+        if not self.answers:
+            print("No slides were found")
+            return
+        else:
+            print("Slides that can be found are")
+            print(self.answers)
+            return
+        #
         # TODO perhpas make all lowercase
+
     def open(self,idx = 0): #open up that website
         if not self.answers:
             raise FileNotFoundError("Lookup not performed in order to open")
-
+        if idx >= len(self.answers): # idx too big
+            raise IndexError("index out of bounds")
         webbrowser.open("{}{}#/{}/{}".format(self.url, self.answers[idx][2], self.answers[idx][0], self.answers[idx][1]))
 
